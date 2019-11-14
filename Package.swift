837199bf41791f9916c0037755c337d1a816eacd
David Azancot <david.azancot@4d.com>
2019-11-14 15:51:34 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "4d-for-ios-form-list-Profile",
+    name: "4d-for-ios-form-list-InvoiceLineList",
     platforms: [
         .iOS(.v13)
     ],
